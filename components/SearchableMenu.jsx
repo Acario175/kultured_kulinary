20f1b1f7970514eab21c0b20f16991eb6a0b2788
Acario175 <krichards175@gmail.com>
2023-12-11 18:31:04 -0500

    added new file

---
+++ components/SearchableMenu.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+// import Select from 'react-select';
+import { useTheme } from '@chakra-ui/react';
+
+import dynamic from 'next/dynamic';
+const Select = dynamic(() => import('react-select'), {
+  ssr: false,
+});
+
+const options = [
+  { label: 'Option 1', value: 'option1' },
+  { label: 'Option 2', value: 'option2' },
+  { label: 'Option 3', value: 'option3' },
+  // Add more options as needed
+];
+
+function SearchableMenu() {
+  const [inputValue, setInputValue] = useState('');
+  const [selectedOption, setSelectedOption] = useState(null);
+  const theme = useTheme();
+
+  const handleOptionClick = (selectedOption) => {
+    console.log(selectedOption.value);
+    setSelectedOption(null); // Reset select option to default
+  };
+
+  const filterOptions = (inputValue) => {
+    return options.filter((option) =>
+      option.label.toLowerCase().includes(inputValue.toLowerCase())
+    );
+  };
+
+  const customStyles = {
+    control: (styles) => ({
+      ...styles,
+      backgroundColor: theme.colors.gray[100],
+    }),
+  };
+
+  return (
+    <Select
+      suppressHydrationWarning={true}
+      //   id='fakeadsd'
+      //   aria-labelledby='aria-label'
+      inputId='aria-example-input'
+      name='aria-live-color'
+      instanceId={'sdfgxcnh'}
+      options={filterOptions(inputValue)}
+      value={selectedOption}
+      onChange={(selectedOption) => {
+        handleOptionClick(selectedOption);
+        setSelectedOption(selectedOption);
+      }}
+      onInputChange={(inputValue) => setInputValue(inputValue)}
+      placeholder='Search options'
+      styles={customStyles}
+      isClearable
+    />
+  );
+}
+
+// export default SearchableInputWithOptions;
+
+export default SearchableMenu;
