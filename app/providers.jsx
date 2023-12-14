2b60b2e223309f44a946911c786d83779f3b0904
Acario175 <krichards175@gmail.com>
2023-12-14 10:37:01 -0500

    Spilt the theme file to its own and imported into provir

--- app/providers.jsx
+++ app/providers.jsx
@@ -1,37 +1,38 @@
 'use client';
 
 import { CacheProvider } from '@chakra-ui/next-js';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import Navbar from '@/components/Navbar';
+import theme from '../lib/theme.js';
 
-const customTheme = extendTheme({
-  components: {
-    Button: {
-      // baseStyle: {
-      //   fontWeight: 'bold',
-      //   borderRadius: 'md',
-      //   _hover: { bg: 'teal.500' },
-      //   _focus: { boxShadow: 'outline' },
-      // },
-      variants: {
-        primary: {
-          bg: '#9656a1',
-          color: 'white',
-          _hover: { bg: '#9656a1' },
-        },
-        secondary: {
-          bg: 'gray.300',
-          _hover: { bg: 'gray.400' },
-        },
-      },
-    },
-  },
-});
+// const customTheme = extendTheme({
+//   components: {
+//     Button: {
+//       // baseStyle: {
+//       //   fontWeight: 'bold',
+//       //   borderRadius: 'md',
+//       //   _hover: { bg: 'teal.500' },
+//       //   _focus: { boxShadow: 'outline' },
+//       // },
+//       variants: {
+//         primary: {
+//           bg: '#9656a1',
+//           color: 'white',
+//           _hover: { bg: '#9656a1' },
+//         },
+//         secondary: {
+//           bg: 'gray.300',
+//           _hover: { bg: 'gray.400' },
+//         },
+//       },
+//     },
+//   },
+// });
 
 export function Providers({ children }) {
   return (
     <CacheProvider>
-      <ChakraProvider theme={customTheme}>
+      <ChakraProvider theme={theme}>
         <Navbar />
 
         {children}
